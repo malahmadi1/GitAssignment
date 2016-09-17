91309433bb2dd72c9ebf2fa279a55348900e3ba6
malahmadi1 <h8.9@hotmail.com>
2016-09-17 17:36:22 -0400

    Editing RoleGame class to include the new weapons

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.fsu.cs.cen5035;
+package assignment1_se;
 
 /**
  *
@@ -12,12 +12,31 @@ package edu.fsu.cs.cen5035;
 public class RoleGame {
 
     public static void main(String[] args) {
+        //creating a new object type sword,
+        //the sword class will call BasicWeapon and pass 50 for a damage 
+        
         Weapon sword = WeaponFactory.getWeapon("sword");
         System.out.println("Sword has " + sword.hit() + " of damage.");
         int armor = 20;
+        //hit will have 50-30 = 20 
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        
-        //TODO: Add the remainig weapons here
-
+   
+       
+      
+         Weapon arrow = WeaponFactory.getWeapon("arrow");
+         
+         System.out.println("Arrow has " + arrow.hit() + " of damage.");
+         System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points, ignoring 5 of them.");
+   
+         //damage 60
+          Weapon axe = WeaponFactory.getWeapon("axe");
+          System.out.println("Axe has " + axe.hit() + " of damage.");
+          System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points, ignoring armor points if it's greater than 0 and less than 20 ");
+   
+          //damage 80
+          Weapon magic = WeaponFactory.getWeapon("magicstaff");
+         System.out.println("MagicStaff has " + magic.hit() + " of damage.");
+        System.out.println("MagicStaff was able to do " + magic.hit(armor) + " of damage due to an armor with "+armor+ " points, ignoring 20% of armor points");
+   
     }
 }
